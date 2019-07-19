654dce5a66d524a4565f336885480ad61d67d5d2
zhaobw <1964471757@qq.com>
2019-07-19 22:35:57 +0800

    complier

--- bin/zf-pack.js
+++ bin/zf-pack.js
@@ -1,3 +1,14 @@
 #! /usr/bin/env node
 
-console.log('start');
+// 1.需要找到当前在执行名的路径，拿到webpack.js
+
+let path = require('path');
+
+ // config 配置文件
+
+ let config = require(path.resolve(__dirname));
+
+ let Compiler = require('../lib/Compiler.js');
+ let compiler = new Compiler(config);
+//  表示运行
+ compiler.run();
